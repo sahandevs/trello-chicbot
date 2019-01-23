eb46e8e992b3a533469321b34dd8fa88f32d7ac0
sahandevs <sahandevs@gmail.com>
2019-01-23 15:28:40 +0330

    supports duplicate labels in labels_to_change_with_label_mapping_labels

--- run.py
+++ run.py
@@ -194,7 +194,10 @@ class Bot:
     for label in labels_to_remove:
       card.remove_label(Label(self.client, label, '?'))
       if change_label_mapping.get(label, None):
-        card.add_label(Label(self.client, change_label_mapping.get(label, None), ''))
+        try:
+          card.add_label(Label(self.client, change_label_mapping.get(label, None), ''))
+        except:
+          pass
 
   def run_task_move(self, card: Card, to_list: trello.List):
     card_labels = card.labels
